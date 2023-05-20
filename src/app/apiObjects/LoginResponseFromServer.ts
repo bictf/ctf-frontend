700c37d406f8cf8b852f28de4752e8a9a01ce6db
Lior Shahar - Ben Ami <lior.sheva103@gmail.com>
2023-05-20 11:38:36 +0000

    Revert "Merge branch 'data-screen' into 'main'"
    This reverts merge request !1

--- src/app/apiObjects/LoginResponseFromServer.ts
+++
@@ -1,4 +0,0 @@
-export type LoginResponseFromServer = {
-  success: Boolean;
-  passwordDiff: Number[];
-}
--- src/app/modules/openapi/services.ts
+++
@@ -1,2 +0,0 @@
-export { LoginService } from './services/login.service';
-export { SearchService } from './services/search.service';
