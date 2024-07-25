15a61f1557cf29efa125802264a7f3705ff4501f
rhron255 <ronhasleton@gmail.com>
2024-07-25 23:23:01 +0300

    Changed animation for header. Made results sort by whether the level is correct. Levels now track their ID (based on ordering from backend). Made button text and functionality rollback on errors from download. Fixed errors not displaying for download issues.

--- src/app/components/password-level/password-game/password-game.component.ts
+++ src/app/components/password-level/password-game/password-game.component.ts
@@ -1,11 +1,10 @@
-import {Component} from '@angular/core';
-import {PasswordInputComponent} from '../password-input/password-input.component';
-import {PasswordGameService} from 'src/app/modules/openapi/services';
-import {MatSnackBar} from '@angular/material/snack-bar';
-import {MatDialog} from '@angular/material/dialog';
-import {NgForOf} from "@angular/common";
-import {RuleBoxComponent} from "../rule-box/rule-box.component";
-import {PasswordLevelData} from "../PasswordLevelData";
+import { Component } from '@angular/core';
+import { PasswordInputComponent } from '../password-input/password-input.component';
+import { DownloadService , PasswordGameService} from 'src/app/modules/openapi/services';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { NgForOf } from "@angular/common";
+import { RuleBoxComponent } from "../rule-box/rule-box.component";
+import { PasswordLevelData } from "../PasswordLevelData";
 
 @Component({
   selector: 'app-password-game',
@@ -15,29 +14,30 @@ import {PasswordLevelData} from "../PasswordLevelData";
   styleUrl: './password-game.component.scss'
 })
 export class PasswordGameComponent {
-  // TODO Sort this by failed first
-  currentRules: Array<PasswordLevelData> = []
-  currentRuleId = 0
-  buttonText: string = "SUBMIT";
+  private readonly SUBMIT_TEXT = "SUBMIT";
+  private readonly DOWNLOAD_TEXT = "DOWNLOAD!!!!!!!!!";
+  protected readonly Array = Array;
 
-  constructor(private dialog: MatDialog, private snackBar: MatSnackBar, private passwordGameApiService: PasswordGameService) {
-    this.checkAnswer("")
+  currentRules: Map<number, PasswordLevelData> = new Map<number, PasswordLevelData>()
+  currentRuleId = 0
+  buttonText: string = this.SUBMIT_TEXT;
+  onSubmitCallback = this.checkAnswer
+
+  constructor(private snackBar: MatSnackBar, private passwordGameApiService: PasswordGameService, private downloadFileService: DownloadService) {
   }
 
   checkAnswer(answer: string) {
     console.log(answer)
-    let levels = this.currentRules.length == 0 ? 1 : this.currentRules.filter((it) => (it.isCorrect)).length + 1;
+    let levels = this.currentRules.size == 0 ? 1 : Array.from(this.currentRules.values()).filter(it => (it.isCorrect)).length + 1;
     this.passwordGameApiService.solve({password: answer, levels: levels}).subscribe(
       (result) => {
         console.log(result)
-        this.currentRules = result.map((item) => {
-          return new PasswordLevelData(item.description, item.isCorrect)
-        }) as PasswordLevelData[]
+        result.forEach((item, index) => {
+          this.currentRules.set(index, new PasswordLevelData(item.description, item.isCorrect))
+        })
         this.currentRuleId = result.length
 
-        if (this.currentRules.every((it) => {
-          return it.isCorrect
-        })) {
+        if (Array.from(this.currentRules.values()).every((it: PasswordLevelData) => it.isCorrect)) {
           this.checkIfFinished(answer)
         }
       },
@@ -48,22 +48,53 @@ export class PasswordGameComponent {
     // TODO: make sure this regenerates screen
   }
 
-  getIdentity(index: number) {
-    return index
-  }
 
   private checkIfFinished(answer: string) {
     this.passwordGameApiService.doesSolveAll({password: answer}).subscribe(
       (result) => {
         if (result) {
-          this.buttonText = "DOWNLOAD!!!!!!!!!"
+          this.buttonText = this.DOWNLOAD_TEXT
+          this.onSubmitCallback = this.downloadFile;
         } else {
           this.checkAnswer(answer)
         }
       },
       (error) => {
-        this.snackBar.open(error.error, '', {duration: 3000, panelClass: 'error-snack-bar'})
+        if (this.snackBar._openedSnackBarRef === undefined) {
+          this.snackBar.open(error.error, '', {duration: 3000, panelClass: 'error-snack-bar'})
+        }
       }
     )
+  }
+
+  getSortedRules() {
+    return Array.from(this.currentRules.entries()).sort((first, second) => {
+      return first[1].isCorrect ? (second[1].isCorrect ? 0 : 1) : -1
+    })
+  }
+
+  downloadFile(password: string) {
+    this.downloadFileService.downloadBinaryFile({password: password, fileName: "Tob Secret File"})
+      .subscribe((data) => {
+          const blob = new File([data], "TobSecretFile.zib", {type: 'application/zip'});
+
+          // Create a link element
+          const link = document.createElement('a');
+          const url = window.URL.createObjectURL(blob);
+          link.href = url;
+          link.download = 'TobSecretFile.zip';
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+          window.URL.revokeObjectURL(url);
+        },
+        async error => {
+          this.snackBar.open(await (error.error as Blob).text(), '', {duration: 3000, panelClass: 'error-snack-bar'})
+
+          this.buttonText = this.SUBMIT_TEXT
+          this.onSubmitCallback = this.checkAnswer
+          this.checkAnswer(password)
+        })
+
   }
 }
