7b38b346e59c45f1f6cb527c96ce637668e4fee7
RoyRoyKing <roy.safranos@gmail.com>
2024-08-07 23:43:22 +0300

    Added working burning mechanism (fuck unicode (but not really I like it this was interesting))

--- src/app/components/password-level/password-input/password-input.component.ts
+++ src/app/components/password-level/password-input/password-input.component.ts
@@ -1,9 +1,9 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { MatFormField, MatLabel } from '@angular/material/form-field';
-import { MatButton } from "@angular/material/button";
-import { MatInput } from "@angular/material/input";
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
-import { MatOption, MatSelect } from "@angular/material/select";
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatButton} from "@angular/material/button";
+import {MatInput} from "@angular/material/input";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatOption, MatSelect} from "@angular/material/select";
 
 @Component({
   selector: 'app-password-input',
@@ -22,36 +22,19 @@ import { MatOption, MatSelect } from "@angular/material/select";
   styleUrl: './password-input.component.scss',
 })
 export class PasswordInputComponent {
+  FIRE_EMOJI = '🔥'
+  FIRE_SPREAD_INTERVAL_IN_MS = 1000
+
   @Input() ruleId?: number
   @Input() rule?: string
   @Output() onSubmit: EventEmitter<string> = new EventEmitter<string>()
-  public passwordForm: FormGroup;
-  @Input() buttonText!: string;
-  private fireTask;
+  public passwordForm: FormGroup
+  @Input() buttonText!: string
 
   constructor(formBuilder: FormBuilder) {
     this.passwordForm = formBuilder.group({
       password: ['', Validators.required]
     })
-    this.fireTask = setInterval(() => {
-      let password = this.getPassword();
-      let newPassword = [];
-      let index = 0;
-      while (index < password.length) {
-        if (password[index] === "🔥") {
-            // Replace a single 🔥 with three 🔥
-            newPassword.pop();  // Remove the last character if it was added
-            newPassword.push("🔥");
-            newPassword.push("🔥");
-            newPassword.push("🔥");
-            index += 2;  // Move past the 🔥
-        } else {
-            newPassword.push(password[index]);
-            index += 1;  // Move to the next character
-        }
-    }
-    this.setPassword(newPassword.join(""))
-    }, 1000)
   }
 
   getPassword() {
@@ -59,6 +42,9 @@ export class PasswordInputComponent {
   }
 
   setPassword(newPassword: string) {
+    for (const char of newPassword) {
+
+    }
     this.passwordForm.get('password')?.setValue(newPassword)
   }
 
@@ -66,10 +52,48 @@ export class PasswordInputComponent {
     this.onSubmit.emit(this.getPassword())
   }
 
-  startFire() {
+  burnPassword() {
+    this.initiatePasswordFire()
+
+    let fireSpreadInterval = setInterval(() => {
+      if (this.isOnFire(this.getPassword())) {
+        this.spreadFireInPassword();
+      } else {
+        clearInterval(fireSpreadInterval);
+      }
+    }, this.FIRE_SPREAD_INTERVAL_IN_MS)
+  }
+
+  initiatePasswordFire() {
     let firePassword = this.getPassword()
-    let randomIndex = Math.round((Math.random() * firePassword.length) + 1)
-    firePassword = firePassword.substring(0, randomIndex) + "🔥" + firePassword.substring(0, randomIndex)
-    this.setPassword(firePassword)
+
+    let fireEndIndex = Math.round((Math.random() * firePassword.length) + 1)
+    firePassword = firePassword.substring(0, fireEndIndex) + this.FIRE_EMOJI + firePassword.substring(fireEndIndex + 1);
+    this.setPassword(firePassword);
+  }
+
+  spreadFireInPassword() {
+    let firePassword = this.getPassword();
+
+    firePassword = this.spreadFireIfLit(firePassword);
+    this.setPassword(firePassword);
+  }
+
+  spreadFireIfLit(burningText: string): string {
+    if (this.isOnFire(burningText)) {
+      let firstFireIndex: number = burningText.indexOf(this.FIRE_EMOJI);
+      let lastFireIndex: number = burningText.lastIndexOf(this.FIRE_EMOJI);
+
+      return burningText.substring(0, firstFireIndex - 1)
+        + this.FIRE_EMOJI
+        + burningText.substring(firstFireIndex, lastFireIndex)
+        + this.FIRE_EMOJI
+        + burningText.substring(lastFireIndex + 3); // +3 because 🔥 is a unicode character. Not sure why its 3 and not 4, as unicode characters take 4 bytes, but it took me 2.5 hours to get it working and it's currently 23:41 so you're not getting an explanation today.
+    }
+    return burningText;
+  }
+
+  isOnFire(text: string): boolean {
+    return text.includes(this.FIRE_EMOJI);
   }
 }
