657a2ff9c1638055c0a36a5c6b1172fc9da3a62d
Danko-PC <dankowiki@gmail.com>
2025-08-01 14:21:38 +0300

    42 change snackbar message for access denied to be displayed on access denied page and to be received from backend

--- src/app/screens/access-denied-screen/access-denied-screen.component.ts
+++ src/app/screens/access-denied-screen/access-denied-screen.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {AccessDeniedService} from "../../modules/openapi/services/access-denied.service"
 
 @Component({
   selector: 'app-access-denied-screen',
   templateUrl: './access-denied-screen.component.html',
   styleUrls: ['./access-denied-screen.component.scss']
 })
-export class AccessDeniedScreenComponent {
+export class AccessDeniedScreenComponent implements OnInit {
+
+  message: string = ''
+
+  constructor(
+    private accessDeniedService: AccessDeniedService,
+    private snackBar: MatSnackBar
+  ) {
+  }
+
+  ngOnInit() {
+    this.accessDeniedService.accessDeniedMessageGet().subscribe(
+      (result) => {
+        this.message = result;
+        this.snackBar.open(this.message, '', {
+          duration: 3000,
+          panelClass: 'error-snack-bar',
+        })
+      }, (error) => {
+        this.message = error.message;
+        this.snackBar.open(this.message, '', {
+          duration: 3000,
+          panelClass: 'error-snack-bar',
+        })
+      }
+    )
+
+
+
+  }
 
 }
